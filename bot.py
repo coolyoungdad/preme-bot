9dc4e24a68bd9c9d75917105d9f7102d31ca8d0f
Alexander Bercow <alexander@bercow.com>
2018-10-31 15:18:53 -0400

    fixed credit card information, added agree to ToS

--- bot.py
+++ bot.py
@@ -15,16 +15,16 @@ def timeme(method):
  
 
 def order (k):  
-    #open URL
+    #opens supreme
     driver.get(k["product_url"]) 
 
-    #click buttons 
+    #click buttons on item page
     driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click() 
     time.sleep(0.5)
     driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
     
     #add keys to cart page
-    #add shipping+billing  
+    #add shipping 
     driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(k["name"]) 
     driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(k["email"]) 
     driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(k["tel"])
@@ -32,10 +32,15 @@ def order (k):
     driver.find_element_by_xpath('//*[@id="oba3"]').send_keys(k["apt"]) 
     driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(k["zip"]) 
     driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(k["city"])
+    driver.find_element_by_xpath('//*[@id="order_billing_state"]/option[37]').click()
 
     #add CC info
-    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(k["cc"]) 
+    driver.find_element_by_id('nnaerb').send_keys(keys['card_number'])
+    driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(k["card_cvv"])
 
+    #click agree to terms
+    process_payment = driver.find_element_by_xpath('//*[@id="pay"]/input')
+    process_payment.click()
 
 if __name__ == '__main__':
     #launch chrome driver 
