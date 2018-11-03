a577cf5280fd8b5940756c92e44f2916829fe41e
Alexander Bercow <alexander@bercow.com>
2018-11-02 21:18:02 -0400

    blank config

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 #keys for the supreme bot 
-
 keys = {
     "product_url": "https://www.supremenewyork.com/shop/all/",
-    "name": "Alexander Bercow",
-    "email": "alexander@bercow.com",
-    "tel": "650 704 0795",
-    "address": "400 Ocean Ave",
-    "apt": "1A",
-    "zip":"11226",
-    "city":"Brooklyn",
-    "card_number": "0000 0000 0000 0000",
-    "card_cvv": "000"
+    "name": "",
+    "email": "",
+    "tel": "",
+    "address": "",
+    "apt": "",
+    "zip":"",
+    "city":"",
+    "card_number": "",
+    "card_cvv": ""
 }
